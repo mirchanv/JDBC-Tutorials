d52f4c313dde536cd65fea7293639f046779785f
mirchanv <137283854+mirchanv@users.noreply.github.com>
2024-09-01 18:29:00 +0200

    Add files via upload

---
+++ 03. JDBC Programs with ResultSet Types/05. Delete Records Using Updatable ResultSet/src/com/mirchanv/jdbc_resultset/UpdatableResultSetDemo.java
@@ -0,0 +1,41 @@
+package com.mirchanv.jdbc_resultset;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class UpdatableResultSetDemo {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/durgadb";
+        String username = "root";
+        String password = "rootroot";
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             Connection conn = DriverManager.getConnection(url, username, password);
+             Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+             ResultSet rs = stmt.executeQuery("select * from emp1")) {
+
+            System.out.print("Please enter begin salary : ");
+            float beginSal = Float.parseFloat(br.readLine());
+
+            System.out.print("Please enter end salary : ");
+            float endSal = Float.parseFloat(br.readLine());
+
+            while (rs.next()) {
+                int empId = rs.getInt(1);
+                float empSal = rs.getFloat(3);
+
+                if (empSal >= beginSal && empSal <= endSal) {
+                    rs.deleteRow();
+                    System.out.println("Employee " + empId + " has been deleted!");
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
